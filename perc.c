f71135fdefb06fdc50de9da289a036d610c23587
Nathan Graves <ngraves@live.com.au>
2017-11-07 13:02:03 +0800

    Piece Size Test

--- perc.c
+++ perc.c
@@ -854,7 +854,8 @@ int main(int argc , char* argv[]){
   	c.parentPieceID =0;
 	c.clusHeight =100;
 	c.clusWidth =100;
-
+	c.colsOccupied= malloc(sizeof(int) * n);
+	c.rowsOccupied = malloc(sizeof(int) * n);
 	for(int i = 0; i < n; i++){
 	c.colsOccupied[i] = i*2;
 	c.rowsOccupied[i] =i *2;
@@ -965,6 +966,8 @@ int main(int argc , char* argv[]){
 		MPI_Status st;		
      		//cluster test
 		cluster c;
+		c.rowsOccupied =malloc(sizeof(int) *n);
+		c.colsOccupied =malloc(sizeof(int) *n);
 		MPI_Recv(&c, 1 , MPI_cluster, 0,0, MPI_COMM_WORLD,&st);
 		
 		printf("CID %d, Parent ID %d, pID %d Height %d Widtg %d size %d\n",c.clusterID ,c.parentClusID,c.parentPieceID ,c.clusHeight,c.clusWidth,c.clusSize);
