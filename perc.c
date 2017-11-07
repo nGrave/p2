44a6b51cfc022d03eb9badac4b66885598214421
Nathan Graves <ngraves@live.com.au>
2017-11-07 20:15:13 +0800

    Simulation Working test 5000 with 25*6

--- perc.c
+++ perc.c
@@ -562,7 +562,7 @@ int main(int argc , char* argv[]){
 
 
 	//Matrix Seeded By MASTER 		
-	for(int i = 1 ; i < numProcs -1 ; i++){
+	for(int i = 0 ; i < numProcs -1 ; i++){
 	
 		int start = matPartSize * i;
 		int end = start + matPartSize;
